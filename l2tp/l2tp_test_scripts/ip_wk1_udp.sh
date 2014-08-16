8c70101ed2eda6030fa5006be3b2bdce889c247a
Andy Zhou <azhou@nicira.com>
2014-08-16 00:04:26 -0700

    fix ip l2tp scripts

--- l2tp/l2tp_test_scripts/ip_wk1_udp.sh
+++ l2tp/l2tp_test_scripts/ip_wk1_udp.sh
@@ -1,36 +1,42 @@
 setup_link()
 {
-   ip link set l2tpeth0 up mtu 1488
-   ifconfig l2tpeth0 7.1.1.1 netmask 255.255.255.0
+	ip link set l2tpeth0 up mtu 1488
+	ip addr add 7.1.1.1 peer 7.1.1.2 dev l2tpeth0
 }
 
 stop_link()
 {
-   ifconfig l2tpeth0 0
+	ip l2tp del session tunnel_id 4000 session_id 2000 
+	ip l2tp del tunnel tunnel_id 4000 
+	ifconfig l2tpeth0 0
 }
 
 start_udp()
 {
-   ip l2tp add tunnel tunnel_id 4000 peer_tunnel_id 3000 encap udp local 10.1.1.1 remote 10.1.1.2 udp_sport 6000 udp_dport 5000
-   ip l2tp add session tunnel_id 4000 session_id 2000 peer_session_id 1000
-   setup_link
+	ip l2tp add tunnel tunnel_id 4000 peer_tunnel_id 3000 encap udp local 10.1.1.1 remote 10.1.1.2 udp_sport 6000 udp_dport 5000
+
+	ip l2tp add session tunnel_id 4000 session_id 2000 peer_session_id 1000
+
+	setup_link
 }
 
 stop_udp()
 {
-   ip l2tp del session tunnel_id 4000 session_id 2000 
-   stop_link
+	stop_link
 }
 
 start_ip()
 {
-	ip addr add 7.1.1.1 peer 7.1.1.2 dev l2tpeth0
-	ip link set l2tpeth0 up mtu 1488
+	ip l2tp add tunnel tunnel_id 4000 peer_tunnel_id 3000 encap ip local 10.1.1.1 remote 10.1.1.2 
+
+	ip l2tp add session tunnel_id 4000 session_id 2000 peer_session_id 1000
+
+	setup_link
 }
 
 stop_ip()
 {
-  echo "stop ip "
+	stop_link
 }
 
 case $1 in 
