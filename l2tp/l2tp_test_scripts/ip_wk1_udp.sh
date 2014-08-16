9147ae95986aa0b0daac080bdc916a8981c8b72f
Andy Zhou <azhou@nicira.com>
2014-08-15 23:48:01 -0700

    ip_wk_udp.sh add parameters

--- l2tp/l2tp_test_scripts/ip_wk1_udp.sh
+++ l2tp/l2tp_test_scripts/ip_wk1_udp.sh
@@ -1,3 +1,57 @@
-ip l2tp add tunnel tunnel_id 4000 peer_tunnel_id 3000 encap udp local 10.1.1.1 remote 10.1.1.2 udp_sport 6000 udp_dport 5000
-ip l2tp add session tunnel_id 4000 session_id 2000 peer_session_id 2000
-ip link set l2tpeth0 up mtu 1488
+setup_link()
+{
+   ip link set l2tpeth0 up mtu 1488
+   ifconfig l2tpeth0 7.1.1.1 netmask 255.255.255.0
+}
+
+stop_link()
+{
+   ifconfig l2tpeth0 0
+}
+
+start_udp()
+{
+   ip l2tp add tunnel tunnel_id 4000 peer_tunnel_id 3000 encap udp local 10.1.1.1 remote 10.1.1.2 udp_sport 6000 udp_dport 5000
+   ip l2tp add session tunnel_id 4000 session_id 2000 peer_session_id 1000
+   setup_link
+}
+
+stop_udp()
+{
+   ip l2tp del session tunnel_id 4000 session_id 2000 
+   stop_link
+}
+
+start_ip()
+{
+	ip addr add 7.1.1.1 peer 7.1.1.2 dev l2tpeth0
+	ip link set l2tpeth0 up mtu 1488
+}
+
+stop_ip()
+{
+  echo "stop ip "
+}
+
+case $1 in 
+	--start-udp)
+		start_udp
+	shift
+	;;
+	--start-ip)
+		start_ip
+	shift
+	;;
+	--stop-udp)
+		stop_udp
+	shift
+	;;
+	--stop-ip)
+		stop_ip
+	shift
+	;;
+	*)
+	shift
+	;;
+esac
+
