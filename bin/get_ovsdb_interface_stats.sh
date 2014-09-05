6173dba22503cf32500a1399502eac4034951fd3
Andy Zhou <azhou@nicira.com>
2014-09-05 15:22:29 -0700

    enhance bashrc

--- bin/get_ovsdb_interface_stats.sh
+++ bin/get_ovsdb_interface_stats.sh
@@ -1,7 +1,7 @@
 #
 # Usage get_ovsdb_interface_stats.sh <interface name.
 
-ovs-vsctl list interface $1 |grep statistics |tr , '\n' |grep rx_packets > /tmp/t1
+ovs-vsctl list interface $1 |grep statistics |tr , '\n' |grep tx_packets > /tmp/t1
 cat /tmp/t1 |sed s/.*rx_packets=//g > /tmp/t2
 cat /tmp/t2
 
