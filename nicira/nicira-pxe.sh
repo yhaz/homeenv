6c4cd00d74b5ab68eaf86f602c169a54b8a85eeb
Andy Zhou <azhou@nicira.com>
2015-07-10 16:11:27 -0700

    add more information on pxe.sh

--- nicira/nicira-pxe.sh
+++ nicira/nicira-pxe.sh
@@ -1,4 +1,7 @@
 
+echo "image is hosted at: http://pxeserver.nicira.eng.vmware.com/"
+echo "passwd is cashcow"
+
 mac=$1
 ssh pxeuser@suite.eng.vmware.com ./PXEconfig.pl -d pa-group/nicira -f tftpboot/pxelinux.cfg/default -m $mac -l HTB -p
 
