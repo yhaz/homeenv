82e94f2cb61e9906148a69033a03a29de1afda6e
Andy Zhou <azhou@ovn.org>
2016-11-03 02:40:11 -0700

    first json parser

--- GO/json/ovsdb_schema.go
+++ GO/json/ovsdb_schema.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 func main() {
-	readFile(os.Args[1])
+	readSchemaFile(os.Args[1])
 }
 
 func readFile(fn string) error {
@@ -16,6 +18,27 @@ func readFile(fn string) error {
 	} else {
 		fmt.Printf("%s", data)
 	}
+
+	return nil
+}
+
+type  oVSDBSchema struct {
+	Name string 
+	Version string
+	Cksum string
+}
+
+func readSchemaFile(fn string) error {
+	file, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return err
+	}
+
+	var s oVSDBSchema
+	if err := json.Unmarshal(file, &s); err != nil {
+		log.Fatalf("Json unmarshaling failed: %s", err)
+	}
+	fmt.Printf("Results: %s, %s, %s\n", s.Name, s.Version, s.Cksum);
 
 	return nil
 }
