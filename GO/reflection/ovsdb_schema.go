faf0b228b6b7169a1da755a0bf7798fe900f33d7
Andy Zhou <azhou@ovn.org>
2016-11-10 03:15:19 -0800

    more reflection

--- GO/reflection/ovsdb_schema.go
+++ GO/reflection/ovsdb_schema.go
@@ -6,15 +6,46 @@ import (
 )
 type  oVSDBSchema struct {
 	Name string  `mytag:"1"`
-	Version string
-	Cksum string
+	Version int
+	Cksum  bool
+}
+
+type  oVSDBSchemaJSON struct {
+	Name *string
+	Version *int
+	Cksum *bool
+}
+
+func CloneValue(s interface{}, d interface{}) {
+	x := reflect.ValueOf(s)
+	y := reflect.ValueOf(d)
+	if  x.Kind() == reflect.Ptr && y.Kind() == x.Kind() && !y.IsNil() {
+		y.Elem().Set(x.Elem())
+	} else {
+		fmt.Printf("Don't know about this type\n")
+	}
 }
 
 func main() {
 	var s oVSDBSchema
-	s.Name="A1"
-	s.Version="B2"
-	s.Cksum="C3"
+        var j oVSDBSchemaJSON
+	var v int = 100
+	var t bool = true
+	var u string = "test"
+	j.Name=&u;
+	j.Version= &v
+	j.Cksum=&t
+        exposeFields(&j)
+	s.Name="B1"
+	s.Version=3
+	s.Cksum=false
+
+        exposeFields(&s)
+	fmt.Printf("Start CloneValue\n")
+
+	CloneValue(j.Name, &s.Name)
+	CloneValue(j.Version, &s.Version)
+	CloneValue(j.Cksum, &s.Cksum)
         exposeFields(&s)
 }
 
@@ -23,7 +54,7 @@ func exposeFields(s interface{}) error {
 
 	for  i:=0; i< v.NumField(); i++ {
 		filedInfo := v.Type().Field(i)
-		fmt.Printf("Filed: %s:%s\n", filedInfo.Name, v.Field(i))
+		fmt.Printf("Filed: %s: %v\n", filedInfo.Name, v.Field(i))
 	}
 
 	return nil
