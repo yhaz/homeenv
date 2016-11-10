e4f9c94042d2c2672708064f3d4fdc755ae81e1c
Andy Zhou <azhou@ovn.org>
2016-11-09 16:11:40 -0800

    go client example

--- GO/ovsdb/example_v3.go
+++ GO/ovsdb/example_v3.go
@@ -4,17 +4,33 @@ package main
 
 import (
 	"github.com/coreos/etcd/clientv3"
+
 	"log"
 	"time"
 )
 
 func main() {
 	cli, err := clientv3.New(clientv3.Config{
-	Endpoints:   []string{"localhost:12378", "localhost:22378", "localhost:32378"},
-	DialTimeout: 5 * time.Second,
+		Endpoints:   []string{"localhost:2379"},
+		DialTimeout: 5 * time.Second,
 	})
 	if err != nil {
-	   log.Printf("error")
+		log.Printf("error")
 	}
+	log.Printf("connected")
 	defer cli.Close()
+
+	ops := []clientv3.Op{
+		clientv3.OpPut("put-key", "123"),
+		clientv3.OpGet("put-key"),
+		clientv3.OpPut("put-key", "435"),
+		clientv3.OpPut("message", "test"),
+	}
+
+	for _, op := range ops {
+		if _, err := cli.Do(context.TODO(), op); err != nil {
+			log.Fatal(err)
+		}
+	}
+	log.Printf("done")
 }
